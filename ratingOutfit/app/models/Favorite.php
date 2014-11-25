ecb3d3ba465b55e3a9c9b837a5552336f5e46552
LucienMoor <lucien.moor@gmail.com>
2014-11-25 18:17:44 +0000

    Add an article to favorite and show favorites

--- ratingOutfit/app/models/Favorite.php
+++ ratingOutfit/app/models/Favorite.php
@@ -4,5 +4,18 @@ class Favorite extends Eloquent {
 
 	protected $table = 'Favorites';
 	public $timestamps = false;
+  
+  public static function findArticle($clientID)
+  {
+  $favorites = Favorite::where('user_ID','=',$clientID)->get();
+  $articles=array();
+  foreach($favorites as $key => $value)
+    {
+    $temp=Article::find($value->articles_ID);
+    array_push($articles,$temp);
+    
+  }
+  return $articles;
+}
 
 }
