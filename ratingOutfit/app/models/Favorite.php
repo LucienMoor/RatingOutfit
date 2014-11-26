198cffb92e59b486b6a20d6f66b8ab6f21a7be8f
LucienMoor <lucien.moor@gmail.com>
2014-11-26 18:06:23 +0000

    added the possibility to delete a favorite

--- ratingOutfit/app/models/Favorite.php
+++ ratingOutfit/app/models/Favorite.php
@@ -5,6 +5,14 @@ class Favorite extends Eloquent {
 	protected $table = 'Favorites';
 	public $timestamps = false;
   
+  public static function ifExist($userID,$articleID){
+    $favorite = Favorite::where('user_ID','=',$userID)->where('articles_ID','=',$articleID)->get();
+    return !$favorite->isEmpty();
+  }
+  public static function findID($userID,$articleID){
+    $favorite = Favorite::where('user_ID','=',$userID)->where('articles_ID','=',$articleID)->first();
+    return $favorite->id;
+  }
   public static function findArticle($clientID)
   {
   $favorites = Favorite::where('user_ID','=',$clientID)->get();
