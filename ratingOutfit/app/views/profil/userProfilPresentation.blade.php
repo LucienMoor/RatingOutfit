1ce364594f533e9a7ee7b758eae5d782a4153d16
LeaHuong <lea.demuralt@gmail.com>
2014-12-05 13:29:04 +0000

    add delete button only for me

--- ratingOutfit/app/views/profil/userProfilPresentation.blade.php
+++ ratingOutfit/app/views/profil/userProfilPresentation.blade.php
@@ -14,8 +14,17 @@
    
    {{ HTML::image("pictures/user/$user->picture") }}
     <button type="button">Love</button>
+    <!-- delete the user (uses the destroy method DESTROY /users/{id} -->
+    <div class="text-center">
+     @if(Auth::id()==$user->id)
+      {{ Form::open(array('url' => 'user/' . $user->id)) }}
+      {{ Form::hidden('_method', 'DELETE') }}
+      {{ Form::submit('Delete my profil') }}
+      {{ Form::close() }}
+     @endif
+    </div>
     <h1>{{{$user->pseudo}}}</h1>
-    <h2>{{{$user->presentation}}}</h2>
+    <h2>{{{$user->presentation}}}</h2>  
   </div> 
 
   @include('articleDetail.index')
